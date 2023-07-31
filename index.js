7559e85f43b1cd0630e6e92715c1425a5c8d86b8
Yontemoon <monteyoon1998@gmail.com>
2023-07-31 15:20:39 -0700

    Added for Render

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
+require('dotenv').config()
 const express = require('express')
-const app = express()
+const cors = require('cors');
+const app = express();
+const mongoose = require('mongoose');
+const Note = require('./models/note')
+const password = "DIPF0mJkaq1d9u7a"
+const url = process.env.MONGODB_URI
+
+// `mongodb+srv://monteyoon1998:${password}@notes-database.iv3awuc.mongodb.net/noteApp?retryWrites=true&w=majority`;
+
+// mongoose.set('strictQuery', false);
+// mongoose.connect(url)
+//   .then(result => {
+//   console.log('Connected to MongoDB');
+//   })
+//   .catch((error) => {
+//   console.log("error coinnecting to MongoDB", error.message)
+// })
+
+// const noteSchema = new mongoose.Schema({
+//   content: String,
+//   important: Boolean,
+// })
+
+// noteSchema.set('toJSON', {
+//   transform: (document, returnedObject) => {
+//     returnedObject.id = returnedObject._id.toString()
+//     delete returnedObject._id
+//     delete returnedObject.__v
+//   }
+// })
+
+// const Note = mongoose.model('Note', noteSchema);
+
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -15,6 +48,8 @@ const unknownEndpoint = (request, response) => {
 
 app.use(express.json())
 app.use(requestLogger)
+app.use(cors())
+app.use(express.static('build'))
 
 let notes = [
   {
@@ -40,7 +75,9 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/notes', (req, res) => {
-  res.json(notes)
+  Note.find({}).then(notes => {
+    res.json(notes)
+  })
 })
 
 const generateId = () => {
@@ -53,36 +90,55 @@ const generateId = () => {
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
+  if (body.content === undefined) {
     return response.status(400).json({ 
       error: 'content missing' 
     })
   }
 
-  const note = {
+  // const note = {
+  //   content: body.content,
+  //   important: body.important || false,
+  //   date: new Date(),
+  //   id: generateId(),
+  // }
+
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
+  // notes = notes.concat(note)
+  // response.json(note)
 
-  response.json(note)
+
+  note.save().then(savedNote => {
+    response.json(savedNote);
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
-
-  response.json(note)
+  })
 })
+
+// app.get('/api/notes/:id', (request, response) => {
+//   Note.findById(request.params.id).then(note => {
+//     response.json(note)
+//   })
+
+//   // const id = Number(request.params.id)
+//   // const note = notes.find(note => note.id === id)
+
+//   // if (note) {
+//   //   response.json(note)
+//   // } else {
+//   //   response.status(404).end()
+//   // }
+
+//   // response.json(note)
+// })
 
 app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
@@ -93,7 +149,7 @@ app.delete('/api/notes/:id', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
