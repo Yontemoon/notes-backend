a0e0613cc687ceca5aa5725084d2eb77a79127e9
Yontemoon <monteyoon1998@gmail.com>
2023-08-07 11:42:47 -0700

    Restructured files for better flow.

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true,
+        "es2021": true
+    },
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+        'eqeqeq': "error",
+        'indent': [
+            'error', 2
+        ],
+        'no-trailing-spaces': 'error',
+        'object-curly-spacing': [
+            'error,', 'always'
+        ],
+        'arrow-spacing': [
+            'error', { 'before': true, 'after': true }
+        ],
+        "no-console": 0
+    }
+}
--- index.js
+++ index.js
@@ -1,143 +1,117 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors');
-const app = express();
-// const mongoose = require('mongoose');
 
-const Note = require('./models/note')
+// const express = require('express')
+// const cors = require('cors');
+// const app = express();
 
-// const password = "DIPF0mJkaq1d9u7a"
-// const url = process.env.MONGODB_URI
+// require('dotenv').config()
+// const Note = require('./models/note')
+// const logger = require("./utils/logger")
+// const config = require("./utils/config")
 
-// `mongodb+srv://monteyoon1998:${password}@notes-database.iv3awuc.mongodb.net/noteApp?retryWrites=true&w=majority`;
-
-// mongoose.set('strictQuery', false);
-// mongoose.connect(url)
-//   .then(result => {
-//   console.log('Connected to MongoDB');
-//   })
-//   .catch((error) => {
-//   console.log("error coinnecting to MongoDB", error.message)
-// })
-
-// const noteSchema = new mongoose.Schema({
-//   content: String,
-//   important: Boolean,
-// })
-
-// noteSchema.set('toJSON', {
-//   transform: (document, returnedObject) => {
-//     returnedObject.id = returnedObject._id.toString()
-//     delete returnedObject._id
-//     delete returnedObject.__v
-//   }
-// })
-
-// const Note = mongoose.model('Note', noteSchema);
-
-
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-app.use(express.json())
-app.use(requestLogger)
-app.use(cors())
-app.use(express.static('build'))
-
-let notes = [
-]
-
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
-
-app.get('/api/notes', (req, res) => {
-  Note.find({}).then(notes => {
-    res.json(notes)
-  })
-})
-
-// const generateId = () => {
-//   const maxId = notes.length > 0
-//     ? Math.max(...notes.map(n => n.id))
-//     : 0
-//   return maxId + 1
+// const requestLogger = (request, response, next) => {
+//   console.log('Method:', request.method)
+//   console.log('Path:  ', request.path)
+//   console.log('Body:  ', request.body)
+//   console.log('---')
+//   next()
 // }
 
-app.post('/api/notes', (request, response) => {
-  const body = request.body
+// const unknownEndpoint = (request, response) => {
+//   response.status(404).send({ error: 'unknown endpoint' })
+// }
 
-  if (body.content === undefined) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
-  }
-
-  // const note = {
-  //   content: body.content,
-  //   important: body.important || false,
-  //   date: new Date(),
-  //   id: generateId(),
-  // }
-
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-  })
-
-  // notes = notes.concat(note)
-  // response.json(note)
+// app.use(express.json())
+// app.use(requestLogger)
+// app.use(cors())
+// app.use(express.static('build'))
 
 
-  note.save().then(savedNote => {
-    response.json(savedNote);
-  })
-})
-
-app.get('/api/notes/:id', (request, response) => {
-  console.log(Note)
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
-})
-
-// app.get('/api/notes/:id', (request, response) => {
-//   Note.findById(request.params.id).then(note => {
-//     response.json(note)
-//   })
-
-//   // const id = Number(request.params.id)
-//   // const note = notes.find(note => note.id === id)
-
-//   // if (note) {
-//   //   response.json(note)
-//   // } else {
-//   //   response.status(404).end()
-//   // }
-
-//   // response.json(note)
+// app.get('/', (req, res) => {
+//   res.send('<h1>Hello World!</h1>')
 // })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+// app.get('/api/notes', (req, res) => {
+//   Note.find({}).then(notes => {
+//     res.json(notes)
+//   })
+// })
 
-  response.status(204).end()
-})
 
-app.use(unknownEndpoint)
+// app.post('/api/notes', (request, response, next) => {
+//   const body = request.body
 
-const PORT = process.env.PORT 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+//   if (body.content === undefined) {
+//     return response.status(400).json({ 
+//       error: 'content missing' 
+//     })
+//   }
+
+//   const note = new Note({
+//     content: body.content,
+//     important: body.important || false,
+//   })
+
+//   note.save().then(savedNote => {
+//     response.json(savedNote);
+//   }).catch(error => next(error))
+// })
+
+// app.get('/api/notes/:id', (request, response, next) => {
+//   Note.findById(request.params.id)
+//     .then(note => {
+//       if (note) {
+//         response.json(note)
+//       } else {
+//         response.status(404).end()
+//       }
+//   })
+//   .catch(error => next(error))
+// })
+
+
+// app.delete('/api/notes/:id', (request, response, next) => {
+
+//   Note.findByIdAndRemove(request.params.id)
+//     .then(result => {
+//       response.status(204).end()
+//     })
+//     .catch(error => next(error))
+// })
+
+
+// app.put('/api/notes/:id', (request, response, next) => {
+//   const {content, important} = request.body;
+//   const note = {
+//     content: body.content,
+//     important: body.important,
+//   }
+
+//   Note.findByIdAndUpdate(request.params.id, {content, important}, { new: true, runValidators: true, content: 'query' })
+//     .then(updatedNote => {
+//       response.json(updatedNote)
+//     })
+//     .catch(error => next(error))
+// })
+
+// const errorHandler = (error, request, response, next) => {
+//   console.log(error.name)
+//   console.error(error.message) //wtf
+
+//   if (error.name === 'CastError') {
+//     return response.status(400).send( {error: 'malformatted id'} )
+//   } else if (error.name === "ValidationError") {
+//       return response.status(400).json({error: error.message})
+//   }
+
+//   next(error)
+// }
+
+// app.use(unknownEndpoint)
+// app.use(errorHandler)
+
+const app = require("./app");
+const config = require('./utils/config');
+const logger = require('./utils/logger')
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
